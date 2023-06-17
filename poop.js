44a5e376806ec1c75d176b4bccd5023e2ae65942
Nikola Stamatovic <stamatmail@gmail.com>
2023-06-17 14:11:26 +0200

    sass node_modules resolver

--- poop.js
+++ poop.js
@@ -6,44 +6,94 @@ const http = require('node:http')
 const livereload = require('livereload')
 const path = require('node:path')
 const { pathToFileURL } = require('node:url')
-const resolve = require('resolve')
 const sass = require('sass')
 const serveStatic = require('serve-static')
 
-function getPackageInfo(pkg) {
-  const pkgPath = resolve.sync(`${pkg}/package.json`, {
-    basedir: process.cwd(),
-  });
-  return JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+function readJsonFile(file) {
+  return JSON.parse(fs.readFileSync(file, 'utf-8'))
+}
+
+function pathExists() {
+  return fs.existsSync(path.join(...arguments))
+}
+
+function pathIsDirectory() {
+  return fs.lstatSync(path.join(...arguments)).isDirectory()
+}
+
+function tryToFindFile(filePath, extensions) {
+  const fileExt = extensions.find(ext => fs.existsSync(`${filePath}.${ext}`))
+  if (fileExt) {
+    return `${filePath}.${fileExt}`
+  }
+  return null
+}
+
+function sassImporter(url) {
+  if (fs.existsSync(url)) return null
+  const importPath = path.relative(process.cwd(), path.join(pathToFileURL('node_modules').pathname, url))
+  console.log(fs.existsSync(importPath), importPath)
+  console.log(path.basename(importPath))
+
+  if (!fs.existsSync(importPath)) {
+    const correctFile = tryToFindFile(importPath, ['sass', 'scss', 'css'])
+    if (correctFile) return new URL(correctFile, pathToFileURL('node_modules'))
+  }
+
+  if (pathIsDirectory(importPath) && !pathExists(importPath, 'index.sass') && !pathExists(importPath, 'index.scss')) {
+    console.log('no index.sass or index.scss', pathExists(importPath, 'package.json'))
+
+    const correctFile = tryToFindFile(importPath, ['sass', 'scss', 'css'])
+    if (correctFile) return new URL(correctFile, pathToFileURL('node_modules'))
+
+    if (pathExists(importPath, 'package.json')) {
+      const pkg = readJsonFile(path.join(importPath, 'package.json'))
+
+      if (pkg.sass) {
+        return new URL(path.join(importPath, pkg.sass), pathToFileURL('node_modules'))
+      }
+
+      if (pkg.css) {
+        return new URL(path.join(importPath, pkg.css), pathToFileURL('node_modules'))
+      }
+
+      const basename = path.basename(pkg.main)
+      if (pkg.main && /(\.sass|\.scss|\.css)$/i.test(basename)) {
+        return new URL(path.join(importPath, pkg.main), pathToFileURL('node_modules'))
+      }
+    } else {
+      return null
+    }
+  }
+
+  return new URL(importPath, pathToFileURL('node_modules'))
 }
 
 // Compile Sass to CSS
 const compiledSass = sass.compile('src/scss/index.scss', {
-//  style: 'compressed',
+  style: 'compressed',
   sourceMap: true,
   sourceMapIncludeSources: true,
   importers: [{
-    // An importer that redirects relative URLs starting with "~" to
-    // `node_modules`.
+    // Resolve `node_modules`.
     findFileUrl(url) {
-      if (fs.existsSync(url)) return null;
-      const urlPath = path.join(pathToFileURL('node_modules').pathname, url);
-      console.log(fs.existsSync(path.relative(process.cwd(), urlPath)), path.relative(process.cwd(), urlPath));
-      return new URL(path.relative(process.cwd(), urlPath), pathToFileURL('node_modules'));
+      return sassImporter(url)
     }
   }]
-});
+})
 
-fs.writeFileSync('dist/styles.css.map', JSON.stringify(compiledSass.sourceMap));
-fs.writeFileSync('dist/styles.css', compiledSass.css);
+fs.writeFileSync('dist/styles.css.map', JSON.stringify(compiledSass.sourceMap))
+fs.writeFileSync('dist/styles.css', compiledSass.css)
 
 // Start local server with LiveReload
-const app = connect();
-app.use(serveStatic(__dirname));
-const port = 4040;
+const app = connect()
+app.use(serveStatic(__dirname))
+const port = 4040
 http.createServer(app).listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
-var lrserver = livereload.createServer();
-lrserver.watch(__dirname);
+const lrserver = livereload.createServer()
+lrserver.watch(__dirname)
+
+console.log(lrserver)
