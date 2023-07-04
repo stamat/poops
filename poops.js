f5f7ee54891afbe0902daae5fae7fb55e48dbf63
Nikola Stamatovic <stamatmail@gmail.com>
2023-07-05 00:53:18 +0200

    externalized styles

--- poops.js
+++ poops.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-
-const autoprefixer = require('autoprefixer')
 const { build } = require('esbuild')
 const chokidar = require('chokidar')
 const connect = require('connect')
-const cssnano = require('cssnano')
 const deepmerge = require('deepmerge')
 const helpers = require('./lib/utils/helpers.js')
 const fs = require('node:fs')
@@ -13,11 +10,9 @@ const http = require('node:http')
 const livereload = require('livereload')
 const nunjucks = require('nunjucks')
 const path = require('node:path')
-const { pathToFileURL } = require('node:url')
-const postcss = require('postcss')
-const sass = require('sass')
 const serveStatic = require('serve-static')
 const Style = require('./lib/utils/style.js')
+const Styles = require('./lib/styles.js')
 const Terser = require('terser')
 
 const {
@@ -26,7 +21,6 @@ const {
   mkPath,
   pathForFile,
   insertMinSuffix,
-  buildStyleOutputFilePath,
   buildScriptOutputFilePath,
   fillBannerTemplate,
   buildTime,
@@ -50,116 +44,6 @@ const configPath = path.join(cwd, defaultConfigPath)
 const config = require(configPath)
 
 const style = new Style()
-
-// SCSS/SASS Compiler
-function tryToFindFile(filePath, extensions) {
-  const fileExt = extensions.find(ext => fs.existsSync(`${filePath}.${ext}`))
-  if (fileExt) {
-    return `${filePath}.${fileExt}`
-  }
-  return null
-}
-
-function sassPathResolver(url, resolvePath) {
-  if (fs.existsSync(url)) return new URL(url)
-  const resolvedPath = pathToFileURL(resolvePath)
-  if (!fs.existsSync(resolvedPath.pathname)) return null
-  const importPath = path.relative(cwd, path.join(resolvedPath.pathname, url))
-
-  if (!fs.existsSync(importPath)) {
-    const correctFile = tryToFindFile(importPath, ['sass', 'scss', 'css'])
-    if (correctFile) return new URL(correctFile, resolvedPath)
-  }
-
-  if (pathIsDirectory(importPath) && !pathExists(importPath, 'index.sass') && !pathExists(importPath, 'index.scss')) {
-    const correctFile = tryToFindFile(importPath, ['sass', 'scss', 'css'])
-    if (correctFile) return new URL(correctFile, resolvedPath)
-
-    if (!pathExists(importPath, 'package.json')) return null
-
-    const pkg = require(path.join(importPath, 'package.json'))
-
-    if (pkg.sass) return new URL(path.join(importPath, pkg.sass), resolvedPath)
-    if (pkg.css) return new URL(path.join(importPath, pkg.css), resolvedPath)
-
-    const basename = path.basename(pkg.main)
-    if (pkg.main && /(\.sass|\.scss|\.css)$/i.test(basename)) return new URL(path.join(importPath, pkg.main), resolvedPath)
-    return null
-  }
-
-  return new URL(importPath, resolvedPath)
-}
-
-function compileStyles() {
-  if (!config.styles) return
-  config.styles = Array.isArray(config.styles) ? config.styles : [config.styles]
-  for (const styleEntry of config.styles) {
-    if (styleEntry.in && styleEntry.out && pathExists(styleEntry.in)) {
-      mkPath(styleEntry.out)
-      compileStylesEntry(styleEntry.in, styleEntry.out, styleEntry.options)
-    }
-  }
-}
-
-function compileStylesEntry(infilePath, outfilePath, options = {}) {
-  const opts = {
-    sourceMap: false,
-    sourceMapIncludeSources: false,
-    importers: [{
-      // Resolve `includePaths`.
-      findFileUrl(url) {
-        for (const includePath of config.includePaths) {
-          const resolvedPath = sassPathResolver(url, includePath)
-          if (resolvedPath) return resolvedPath
-        }
-        return null
-      }
-    }]
-  }
-
-  if (options.sourcemap) {
-    opts.sourceMap = options.sourcemap
-    opts.sourceMapIncludeSources = options.sourcemap
-  }
-
-  outfilePath = buildStyleOutputFilePath(infilePath, outfilePath, options)
-
-  const cssStart = performance.now()
-  const compiledSass = sass.compile(infilePath, opts)
-  const mapsrc = options.sourcemap ? `\n/*# sourceMappingURL=${path.basename(outfilePath)}.map */` : ''
-  if (banner) compiledSass.css = banner + '\n' + compiledSass.css
-  fs.writeFileSync(outfilePath, compiledSass.css + mapsrc)
-  const cssEnd = performance.now()
-  if (!options.justMinified) console.log(`${style.magentaBright + style.bold}[style]${style.reset} ${style.dim}Compiled:${style.reset} ${style.italic + style.underline}${outfilePath}${style.reset} ${style.greenBright}${fileSize(outfilePath)}${style.reset} ${style.green}(${buildTime(cssStart, cssEnd)})${style.reset}`)
-
-  if (compiledSass.sourceMap) {
-    if (banner) compiledSass.sourceMap.mappings = ';' + compiledSass.sourceMap.mappings
-    fs.writeFileSync(`${outfilePath}.map`, JSON.stringify(compiledSass.sourceMap))
-    console.log(`${style.magentaBright + style.bold}[style]${style.reset} ${style.dim}Compiled:${style.reset} ${style.italic + style.underline}${outfilePath}.map${style.reset}`)
-  }
-
-  const cssMinStart = performance.now()
-  const minPath = insertMinSuffix(outfilePath)
-  if (options.minify) {
-    postcss([autoprefixer, cssnano]).process(compiledSass.css, {
-      from: outfilePath,
-      to: minPath
-    }).then(result => {
-      if (banner) result.css = banner + '\n' + result.css
-      fs.writeFileSync(minPath, result.css)
-      const cssMinEnd = performance.now()
-      console.log(`${style.magentaBright + style.bold}[style]${style.reset} ${style.dim}Compiled:${style.reset} ${style.italic + style.underline}${minPath}${style.reset} ${style.greenBright}${fileSize(minPath)}${style.reset} ${style.green}(${buildTime(cssMinStart, cssMinEnd)})${style.reset}`)
-    }).catch((error) => {
-      console.log(`${style.redBright + style.bold}[error]${style.reset} Error occurred during CSS minification: ${style.dim}${error}${style.reset}`)
-    })
-
-    if (options.justMinified) {
-      fs.unlinkSync(outfilePath)
-    }
-  } else {
-    fs.unlinkSync(minPath)
-  }
-}
 
 // JS/TS Compiler
 function compileScripts() {
@@ -277,14 +161,14 @@ function poops() {
     lrserver.watch(cwd)
   }
 
-  compileStyles()
+  styles.compile()
   compileScripts()
   compileHTML()
 
   if (config.watch) {
     chokidar.watch(config.watch).on('change', (file) => {
       if (/(\.js|\.ts)$/i.test(file)) compileScripts()
-      if (/(\.sass|\.scss|\.css)$/i.test(file)) compileStyles()
+      if (/(\.sass|\.scss|\.css)$/i.test(file)) styles.compile()
       if (/(\.html|\.njk)$/i.test(file)) compileHTML()
     })
   }
@@ -424,6 +308,8 @@ if (config.includePaths) {
 } else {
   config.includePaths = ['node_modules']
 }
+
+const styles = new Styles(config)
 
 // Start the webserver
 if (config.serve) {
