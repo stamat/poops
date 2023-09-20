db985743a3480c6345029768336d792fba71542b
Nikola Stamatovic <stamatmail@gmail.com>
2023-09-20 20:02:10 +0200

    Build version 1.0.16

--- example/dist/js/scripts.js
+++ example/dist/js/scripts.js
@@ -1,4 +1,4 @@
-/* poops v1.0.15 | https://github.com/stamat/poops | MIT License */
+/* poops v1.0.16 | https://github.com/stamat/poops | MIT License */
 "use strict";
 (() => {
   // example/src/js/scripts/utils.ts
